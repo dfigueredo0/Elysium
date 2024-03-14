c1934e07781d87da0bcaa0b5308edafc05718597
Dylan Figueredo <d.figueredo2104@gmail.com>
2024-03-13 23:27:22 -0500

    Copyright Comment

---
+++ Engine/Common/CommonHeaders.h
@@ -0,0 +1,7 @@
+// Copyright (c) Dylan Figueredo
+// Distributed under the MIT license. See the LICENSE file in the project root for more information.
+#pragma once
+
+// C/C++
+#include <stdint.h>
+#include <assert.h>
