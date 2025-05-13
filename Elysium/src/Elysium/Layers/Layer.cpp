3ad6a4ee47ff73de6b6d2ae8c7c2bde896297274
Dylan Figueredo <d.figueredo2104@gmail.com>
2025-05-12 23:44:32 -0500

    Added Layers

---
+++ Elysium/src/Elysium/Layers/Layer.cpp
@@ -0,0 +1,12 @@
+#include "elypch.h"
+#include "Layer.h"
+
+namespace Elysium {
+	Layer::Layer(const std::string& debugName) : m_DebugName(debugName)
+	{
+	}
+
+	Layer::~Layer()
+	{
+	}
+}
---
+++ Elysium/src/Elysium/Layers/Layer.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Elysium/Core.h"
+#include "Elysium/Events/Event.h"
+
+namespace Elysium {
+	class ELYSIUM_API Layer {
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& e) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+	protected:
+		// TODO: Establish Macros to def/undef for distribution build
+		std::string m_DebugName;
+	};
+}
