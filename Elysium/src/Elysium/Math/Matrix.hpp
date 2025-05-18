e56cbb7163177014e8ded824a870eb5f624a7c6f
Dylan Figueredo <d.figueredo2104@gmail.com>
2025-05-17 22:27:57 -0500

    Euler Angles and Quaternions

--- Elysium/src/Elysium/Math/Matrix.hpp
+++ Elysium/src/Elysium/Math/Matrix.hpp
@@ -146,6 +146,19 @@ namespace Math {
             return inv;
         }
 
+        template<typename T, int R, int C>
+        Matrix<T, C, R> getTransposition(const Matrix<T, R, C>& mat) {
+            Matrix<T, C, R> result;
+
+            for (int i = 0; i < R; ++i) {
+                for (int j = 0; j < C; ++j) {
+                    result(j, i) = mat(i, j);
+                }
+            }
+
+            return result;
+        }
+
 #pragma region TODO
         // TODO: make generic
         INLINE mat4 mat4Translation(vec3 pos) {
@@ -164,11 +177,6 @@ namespace Math {
             return mat;
         }
 
-        INLINE mat4 mat4Euler(float xRad, float yRad, float zRad) {
-
-        }
-#pragma endregion
-
         INLINE vec4 getRow(int i) const {
             vec4 result;
             for (int j = 0; j < 4; ++j)
@@ -180,6 +188,128 @@ namespace Math {
             for (int j = 0; j < 4; ++j)
                 data[i * 4 + j] = row.elements[j];
         }
+#pragma endregion
+
+#pragma region MoveToMat4.inl
+        // Tait-Bryan Angles -- intersection of two non-homologous planes -- most common convention Z(a)X(b)Y(c)
+        // Assuming a frame with unit vectors (the angle theta is negative):
+        //      sin(theta) = -x3                                theta = arcsin(-x3)
+        //      sin(psi) = x2/sqrt(1-(x3)^2)                    psi = arcsin(x2/sqrt(1-(x3)^2))
+        //      sin(phi) = y3/sqrt(1-(x3)^2)                    phi = arcsin(y3/sqrt(1-(x3)^2))
+
+        INLINE constexpr mat4 mat4EulerX(float rad) {
+            mat4 mat = mat4::identity();
+            float c = cos(rad), s = sin(rad);
+
+            mat(1, 1) = c;
+            mat(1, 2) = -s;
+            mat(2, 1) = s;
+            mat(2, 2) = c;
+
+            return mat;
+        }
+        
+        INLINE constexpr mat4 mat4EulerY(float rad) {
+            mat4 mat = mat4::identity();
+            float c = cos(rad), s = sin(rad);
+
+            mat(0, 0) = c;
+            mat(0, 2) = s;
+            mat(2, 0) = -s;
+            mat(2, 2) = c;
+
+            return mat;
+        }
+        
+        INLINE constexpr mat4 mat4EulerZ(float rad) {
+            mat4 mat = mat4::identity();
+            float c = cos(rad), s = sin(rad);
+
+            mat(0, 0) = c;
+            mat(0, 1) = -s;
+            mat(1, 0) = s;
+            mat(1, 1) = c;
+
+            return mat;
+        }
+
+        //Def: ZYX order.
+        INLINE constexpr mat4 mat4Euler(float xRad, float yRad, float zRad) {
+            return mat4EulerX(xRad) * mat4EulerY(yRad) * mat4EulerZ(zRad);
+        }
+
+        /**
+        *   @brief Returns a forward vector relative to the provided matrix.
+        */
+        INLINE vec3 mat4Forward(mat4 mat) {
+            vec3 forward;
+            forward.x = mat.data[2];
+            forward.y = mat.data[6];
+            forward.z = mat.data[10];
+            vec3Normalize(&forward);
+            return forward;
+        }
+
+        /**
+        *   @brief Returns a backward vector relative to the provided matrix.
+        */
+        INLINE vec3 mat4Backward(mat4 mat) {
+            vec3 backward;
+            backward.x = -mat.data[2];
+            backward.y = -mat.data[6];
+            backward.z = -mat.data[10];
+            vec3Normalize(&backward);
+            return backward;
+        }
+
+        /**
+        *   @brief Returns a up vector relative to the provided matrix.
+        */
+        INLINE vec3 mat4Up(mat4 mat) {
+            vec3 up;
+            up.x = mat.data[1];
+            up.y = mat.data[5];
+            up.z = mat.data[9];
+            vec3Normalize(&up);
+            return up;
+        }
+
+        /**
+        *   @brief Returns a down vector relative to the provided matrix.
+        */
+        INLINE vec3 mat4Down(mat4 mat) {
+            vec3 down;
+            down.x = -mat.data[1];
+            down.y = -mat.data[5];
+            down.z = -mat.data[9];
+            vec3Normalize(&down);
+            return down;
+        }
+       
+        /**
+        *   @brief Returns a left vector relative to the provided matrix.
+        */
+        INLINE vec3 mat4Left(mat4 mat) {
+            vec3 left;
+            left.x = -mat.data[0];
+            left.y = -mat.data[4];
+            left.z = -mat.data[8];
+            vec3Normalize(&left);
+            return left;
+        }
+
+        /**
+        *   @brief Returns a right vector relative to the provided matrix.
+        */
+        INLINE vec3 mat4Right(mat4 mat) {
+            vec3 right;
+            right.x = mat.data[0];
+            right.y = mat.data[4];
+            right.z = mat.data[8];
+            vec3Normalize(&right);
+            return right;
+        }
+#pragma endregion
     };
 
     using mat2 = Matrix<float, 2, 2>;
--- Elysium/src/Elysium/Math/Quaternion.hpp
+++ Elysium/src/Elysium/Math/Quaternion.hpp
@@ -1,1 +1,169 @@
 #pragma once
+
+#include "Elysium/Core.h"
+#include "Matrix.hpp"
+#include "Vec4.hpp"
+
+namespace Math {
+	typedef vec4 quat;
+	
+	INLINE quat quatIdentity() {
+		return quat(0, 0, 0, 1.0f);
+	}
+
+	INLINE float quatNormal(quat q) {
+		return fsqrt(
+			q.x * q.x +
+			q.y * q.y +
+			q.z * q.z +
+			q.w * q.w
+		);
+	}
+
+	INLINE quat quatNormalize(quat q) {
+		float norm = (quatNormal(q) <= 0) ? quatNormal(q) : 0.0f;
+		return quat(q.x / norm, q.y / norm, q.z / norm, q.w / norm);
+	}
+
+	INLINE quat quatConjugate(quat q) {
+		return quat(-q.x, -q.y, -q.z, q.w);
+	}
+
+	INLINE quat quatInverse(quat q) {
+		return quatNormalize(quatConjugate(q));
+	}
+
+	INLINE quat quatMul(quat q, quat r) {
+		quat t;
+
+		t.x = r.w * q.x + r.x * q.w - r.y * q.z + r.z * q.y;
+		t.y = r.w * q.y + r.x * q.z + r.y * q.w - r.z * q.x;
+		t.z = r.w * q.z - r.x * q.y + r.y * q.x + r.z * q.w;
+		t.w = r.w * q.w - r.x * q.x - r.y * q.y - r.z * q.z;
+
+		return t;
+	}
+
+	INLINE float quatDot(quat q, quat r) {
+		return q.x * r.x + q.y * r.y + q.z * r.z + q.w * r.w;
+	}
+
+	INLINE mat4 quatToMat4(quat q) {
+		mat4 mat = mat4::identity();
+		quat n = quatNormalize(q);
+
+		float x = n.x, y = n.y, z = n.z, w = n.w;
+		float xx = x * x, yy = y * y, zz = z * z;
+		float xy = x * y, xz = x * z, yz = y * z;
+		float wx = w * x, wy = w * y, wz = w * z;
+
+
+		mat.data[0] = 1.0f - 2.0f * (yy + zz);
+		mat.data[1] = 2.0f * (xy - wz);
+		mat.data[2] = 2.0f * (xz + wy);
+
+		mat.data[4] = 2.0f * (xy + wz);
+		mat.data[5] = 1.0f - 2.0f * (xx + zz);
+		mat.data[6] = 2.0f * (yz - wx);
+
+		mat.data[8] = 2.0f * (xz - wy);
+		mat.data[9] = 2.0f * (yz + wx);
+		mat.data[10] = 1.0f - 2.0f * (xx + yy);
+
+		return mat;
+	}
+
+	INLINE mat4 quatToRotMatrix(quat q, vec3 center) {
+		q = quatNormalize(q);
+
+		float x = q.x, y = q.y, z = q.z, w = q.w;
+		float xx = x * x, yy = y * y, zz = z * z;
+		float xy = x * y, xz = x * z, yz = y * z;
+		float wx = w * x, wy = w * y, wz = w * z;
+
+		mat4 mat = mat4::identity();
+
+		mat(0, 0) = 1.0f - 2.0f * (yy + zz);
+		mat(0, 1) = 2.0f * (xy - wz);
+		mat(0, 2) = 2.0f * (xz + wy);
+		mat(0, 3) = 0.0f;
+
+		mat(1, 0) = 2.0f * (xy + wz);
+		mat(1, 1) = 1.0f - 2.0f * (xx + zz);
+		mat(1, 2) = 2.0f * (yz - wx);
+		mat(1, 3) = 0.0f;
+
+		mat(2, 0) = 2.0f * (xz - wy);
+		mat(2, 1) = 2.0f * (yz + wx);
+		mat(2, 2) = 1.0f - 2.0f * (xx + yy);
+		mat(2, 3) = 0.0f;
+
+		// Compute translation so that the rotation is centered around `center`
+		float tx = center.x - (mat(0, 0) * center.x + mat(0, 1) * center.y + mat(0, 2) * center.z);
+		float ty = center.y - (mat(1, 0) * center.x + mat(1, 1) * center.y + mat(1, 2) * center.z);
+		float tz = center.z - (mat(2, 0) * center.x + mat(2, 1) * center.y + mat(2, 2) * center.z);
+
+		mat(3, 0) = tx;
+		mat(3, 1) = ty;
+		mat(3, 2) = tz;
+		mat(3, 3) = 1.0f;
+
+		return mat;
+	}
+
+	INLINE quat quatFromAxisAngle(vec3 axis, float angle, char normalize) {
+		const float half_angle = 0.5f * angle;
+
+		float s = sin(half_angle);
+		float c = cos(half_angle);
+
+		quat q = quat(s * axis.x, s * axis.y, s * axis.z, c);
+
+		if (normalize) {
+			return quatNormalize(q);
+		}
+
+		return q;
+	}
+
+	/**
+	*
+	*	@param q Starting quaternion value
+	*	@param r Ending quaternion value
+	*	@param t Interpolation ratio. Value range is [0, 1].
+	*/
+	INLINE quat quatSlerp(quat q, quat r, float t) {
+		q = quatNormalize(q);
+		r = quatNormalize(r);
+		quat result;
+		float cosHalfTheta = quatDot(q, r);
+
+		if (cosHalfTheta < 0.0f) {
+			r.x = -r.x;
+			r.y = -r.y;
+			r.z = -r.z;
+			r.w = -r.w;
+			cosHalfTheta = -cosHalfTheta;
+		}
+
+		const float DOT_THRESHOLD = 0.9995f;
+		if (cosHalfTheta > DOT_THRESHOLD) {
+			// Linear interpolation + normalize
+			for (int i = 0; i < 4; ++i)
+				result.elements[i] = q.elements[i] + (r.elements[i] - q.elements[i]) * t;
+			return quatNormalize(result);
+		}
+
+
+		float theta = acosf(cosHalfTheta);
+		float sinTheta = sinf(theta);
+
+		float s0 = sinf((1 - t) * theta) / sinTheta;
+		float s1 = sinf(t * theta) / sinTheta;
+
+		for (int i = 0; i < 4; ++i)
+			result.elements[i] = q.elements[i] * s0 + r.elements[i] * s1;
+
+		return quatNormalize(result);
+	}
+}
